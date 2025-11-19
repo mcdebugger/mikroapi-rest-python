b0a2f9ca0895e3bcb22450a875c00c3f95de414c
Maksim Kachur <me@maksimkachur.ru>
2025-11-19 15:50:15 +0300

    Add "mtu" field to base interface model

--- src/mikroapi/models/interface.py
+++ src/mikroapi/models/interface.py
@@ -1,12 +1,14 @@
+from typing import Literal
 from pydantic import Field
 
 from .base import MikrotikBaseModel
 
 class BaseInterface(MikrotikBaseModel):
-    id: str = Field(alias=".id")
+    id: str = Field(alias='.id')
     name: str
     comment: str | None = None
     disabled: bool
+    mtu: int | Literal['auto']
     running: bool
 
 class Interface(BaseInterface):
