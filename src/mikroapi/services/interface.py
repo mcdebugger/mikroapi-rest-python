4bdc44fef81256432d8650fa3204496194c502bd
Maksim Kachur <me@maksimkachur.ru>
2025-11-19 23:50:46 +0300

    Use CollectionService base class for interface.ethernet

--- src/mikroapi/services/interface.py
+++ src/mikroapi/services/interface.py
@@ -1,13 +1,8 @@
 from ..models.interface import Interface, EthernetInterface
-from .base import BaseService
+from .base import BaseService, CollectionService
 
 class BaseInterfaceService(BaseService):
     pass
-
-class EthernetInterfaceService(BaseInterfaceService):
-    async def all(self) -> list[EthernetInterface]:
-        data = await self.api.get_list('interface/ethernet')
-        return [EthernetInterface(**interface) for interface in data]
 
 class InterfaceService(BaseInterfaceService):
     async def all(self) -> list[Interface]:
@@ -16,4 +11,4 @@ class InterfaceService(BaseInterfaceService):
     
     @property
     def ethernet(self):
-        return EthernetInterfaceService(self.api)
+        return CollectionService(self.api, 'interface/ethernet', EthernetInterface)
