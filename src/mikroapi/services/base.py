828cad4417e4d05d43b0b7414fe0c05b6720d745
Maksim Kachur <me@maksimkachur.ru>
2025-11-19 23:23:29 +0300

    Refactor single item services

--- src/mikroapi/services/base.py
+++ src/mikroapi/services/base.py
@@ -1,5 +1,19 @@
+from typing import TypeVar, Generic
 from ..client import AsyncMikrotikRESTAPIClient
+from ..models.base import MikrotikBaseModel
+
+M = TypeVar('M', bound='MikrotikBaseModel')
 
 class BaseService:
     def __init__(self, api: AsyncMikrotikRESTAPIClient):
         self.api = api
+
+class SingleItemService(BaseService, Generic[M]):
+    def __init__(self, api: AsyncMikrotikRESTAPIClient, endpoint: str, model: type[M]):
+        super().__init__(api)
+        self.endpoint = endpoint
+        self.model = model
+        
+    async def get(self) -> M:
+        data = await self.api.get_item(self.endpoint)
+        return self.model(**data)
--- src/mikroapi/services/system.py
+++ src/mikroapi/services/system.py
@@ -1,32 +1,15 @@
-from .base import BaseService
+from .base import BaseService, SingleItemService
 from ..models.system import SystemIdentity, SystemResource, SystemRouterBoard
 
-class SystemIdentityService(BaseService):
-    async def get(self) -> SystemIdentity:
-        data = await self.api.get_item('system/identity')
-        return SystemIdentity(**data)
-
-class SystemResourceService(BaseService):
-    async def get(self) -> SystemResource:
-        data = await self.api.get_item('system/resource')
-        return SystemResource(**data)
-
-class SystemRouterBoardService(BaseService):
-    async def get(self) -> SystemRouterBoard:
-        data = await self.api.get_item('system/routerboard')
-        return SystemRouterBoard(**data)
-
 class SystemService(BaseService):
-    pass
+    @property
+    def identity(self) -> SingleItemService[SystemIdentity]:
+        return SingleItemService(self.api, 'system/identity', SystemIdentity)
 
     @property
-    def identity(self):
-        return SystemIdentityService(self.api)
-    
+    def resource(self) -> SingleItemService[SystemResource]:
+        return SingleItemService(self.api, 'system/resource', SystemResource)
+
     @property
-    def resource(self):
-        return SystemResourceService(self.api)
-    
-    @property
-    def routerboard(self):
-        return SystemRouterBoardService(self.api)
+    def routerboard(self) -> SingleItemService[SystemRouterBoard]:
+        return SingleItemService(self.api, 'system/routerboard', SystemRouterBoard)
