eff7ba6ba1c462b96ef33ac918c50ec9329df42b
Maksim Kachur <me@maksimkachur.ru>
2025-11-19 20:39:19 +0300

    Add system model to API

--- src/mikroapi/api.py
+++ src/mikroapi/api.py
@@ -1,10 +1,11 @@
 from .client import AsyncMikrotikRESTAPIClient
-from .services import InterfaceService
+from .services import InterfaceService, SystemService
 
 class AsyncMikrotikRESTAPI():
     def __init__(self, **kwargs):
         self._client = AsyncMikrotikRESTAPIClient(**kwargs)
         self.interface = InterfaceService(self._client)
+        self.system = SystemService(self._client)
     
     async def __aenter__(self):
         await self._client.__aenter__()
