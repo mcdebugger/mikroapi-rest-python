b865eeaa9ee56db53edaf2d2ea2f8719fc14ec66
Maksim Kachur <me@maksimkachur.ru>
2025-11-20 00:21:33 +0300

    Add system.package service

--- src/mikroapi/services/system.py
+++ src/mikroapi/services/system.py
@@ -1,11 +1,25 @@
-from .base import BaseService, SingleItemService
-from ..models.system import SystemIdentity, SystemResource, SystemRouterBoard
+from .base import BaseService, CollectionService, SingleItemService
+from ..client import AsyncMikrotikRESTAPIClient
+from ..models.system import SystemIdentity, SystemPackage, SystemPackageUpdate
+from ..models.system import SystemResource, SystemRouterBoard
+
+class SystemPackageService(CollectionService[SystemPackage]):
+    def __init__(self, api: AsyncMikrotikRESTAPIClient):
+        super().__init__(api, 'system/package', SystemPackage)
+    
+    @property
+    def update(self) -> SingleItemService[SystemPackageUpdate]:
+        return SingleItemService(self.api, 'system/package/update', SystemPackageUpdate)
 
 class SystemService(BaseService):
     @property
     def identity(self) -> SingleItemService[SystemIdentity]:
         return SingleItemService(self.api, 'system/identity', SystemIdentity)
 
+    @property
+    def package(self) -> SystemPackageService:
+        return SystemPackageService(self.api)
+    
     @property
     def resource(self) -> SingleItemService[SystemResource]:
         return SingleItemService(self.api, 'system/resource', SystemResource)
