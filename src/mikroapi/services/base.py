fba39f9f58638428beeda51f04868f052ad6ff78
Maksim Kachur <me@maksimkachur.ru>
2025-11-19 23:49:59 +0300

    Add CollectionService base class for list requests

--- src/mikroapi/services/base.py
+++ src/mikroapi/services/base.py
@@ -8,6 +8,16 @@ class BaseService:
     def __init__(self, api: AsyncMikrotikRESTAPIClient):
         self.api = api
 
+class CollectionService(BaseService, Generic[M]):
+    def __init__(self, api: AsyncMikrotikRESTAPIClient, endpoint: str, model: type[M]):
+        super().__init__(api)
+        self.endpoint = endpoint
+        self.model = model
+    
+    async def all(self) -> list[M]:
+        data = await self.api.get_list(self.endpoint)
+        return [self.model(**item) for item in data]
+
 class SingleItemService(BaseService, Generic[M]):
     def __init__(self, api: AsyncMikrotikRESTAPIClient, endpoint: str, model: type[M]):
         super().__init__(api)
