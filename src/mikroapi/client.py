f8ca1ee220bb874f847f122dccaf995386351604
Maksim Kachur <me@maksimkachur.ru>
2026-08-23 15:00:38 +0300

    Add port definition to client

--- src/mikroapi/client.py
+++ src/mikroapi/client.py
@@ -13,11 +13,16 @@ class AsyncMikrotikRESTAPIClient:
         host: str,
         username: str,
         password: str,
+        port: int | None = None,
         use_https: bool = True,
         verify_ssl: bool = False,
         timeout: int = 30,
     ):
-        self.base_url = f"{'https' if use_https else 'http'}://{host}/rest"
+        self.base_url = f"{'https' if use_https else 'http'}://" \
+                        f"{host}" \
+                        f"{'' if port is None else f':{port}'}" \
+                        "/rest"
+        logger.debug(f'Base URL: {self.base_url}')
         self.auth = httpx.BasicAuth(username, password)
         self.verify_ssl = verify_ssl
         self.timeout = timeout
